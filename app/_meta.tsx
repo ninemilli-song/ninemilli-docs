f966905b5156f2c271d623094df584dd8d212d1a
Lebowski <lebowski@LebowskideMacBook-Pro.local>
2025-06-11 10:56:16 +0800

    工程文件搭建

---
+++ app/_meta.tsx
@@ -0,0 +1,16 @@
+const meta = {
+  index: {
+    title: '首页',
+    type: 'page'
+  },
+  docs: {
+    title: '文库',
+    type: 'page'
+  },
+  about: {
+    title: '关于',
+    type: 'page'
+  }
+};
+
+export default meta;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+import nextra from 'nextra'
+ 
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+})
+ 
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  // ... Add regular Next.js options here
+  turbopack: {
+    resolveAlias: {
+      // Path to your `mdx-components` file with extension
+      // 'private-next-root-dir': __dirname,
+      "private-next-root-dir/*": "./*",
+      'next-mdx-import-source-file': './mdx-components.tsx'
+    }
+  }
+})
