ec3b232ce1af08f8fd8c02ced8fcbe01db46a513
ninemilli-song <ninemilli_song@163.com>
2025-06-20 16:44:48 +0800

    Adjust home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
-import { Banner, Head } from 'nextra/components'
+import { Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
 import "./globals.css";
@@ -17,14 +17,14 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "九毫文库",
+  title: "九毫墨痕",
   description: "九毫文库，汇集各种有意思的文档",
 };
 
-const banner = <Banner storageKey="some-key">九毫文库 上线啦 🎉</Banner>
+// const banner = <Banner storageKey="some-key">九毫墨痕 上线啦 🎉</Banner>
 const navbar = (
   <Navbar
-    logo={<b>九毫文库</b>}
+    logo={<b>九毫墨痕</b>}
     // ... Your additional navbar options
   />
 )
@@ -53,7 +53,6 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Layout
-          banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
